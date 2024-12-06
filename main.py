e5fc020b5f28fa410853918e7067c79d6a948636
asilbek <alijonovasilbek058@gmail.com>
2024-12-06 09:45:19 +0500

    10 likdan 16 likka o'tish

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
-# def decimal_to_binary(decimal_number):
-#     if decimal_number == 0:
-#         return '0'
-#
-#     binary_number = ''
-#     while decimal_number > 0:
-#         binary_number = str(decimal_number % 2) + binary_number
-#         decimal_number = decimal_number // 2
-#
-#     return binary_number
-#
-#
-# # Versiya 1: 10-likdan 2-likka o'tish
-# decimal_number = 10
-# print(f"Versiya 1: {decimal_number} soni = {decimal_to_binary(decimal_number)} ikkilikda")
-
-
-# 10-lik raqamni 8-likka o'tkazish (kutubxonalarsiz)
-def decimal_to_octal(decimal_number):
+# 10-lik raqamni 16-likka o'tkazish (kutubxonalarsiz)
+def decimal_to_hexadecimal(decimal_number):
     if decimal_number == 0:
         return '0'
 
-    octal_number = ''
+    hex_digits = '0123456789ABCDEF'
+    hexadecimal_number = ''
     while decimal_number > 0:
-        octal_number = str(decimal_number % 8) + octal_number
-        decimal_number = decimal_number // 8
+        hexadecimal_number = hex_digits[decimal_number % 16] + hexadecimal_number
+        decimal_number = decimal_number // 16
 
-    return octal_number
+    return hexadecimal_number
 
 
-# Versiya 2: 10-likdan 8-likka o'tish
+# Versiya 3: 10-likdan 16-likka o'tish
 decimal_number = 10
-print(f"Versiya 2: {decimal_number} (soni) = {decimal_to_octal(decimal_number)} ( 8 likda)")
-
+print(f"Versiya 3: {decimal_number} (soni) = {decimal_to_hexadecimal(decimal_number)} (16 likda)")
